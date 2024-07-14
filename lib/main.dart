a941f603b0155e65bb87bece64fe98bcbf15f9f0
sanjivkm044 <sanjivkm044@gmail.com>
2024-07-14 15:34:42 +0545

    firebase configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:roomfinder/firebase_options.dart';
 import 'package:roomfinder/presentation/features/navigation/navigation_screen.dart';
 import 'package:roomfinder/theme/theme.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,13 +22,34 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'RoomFinder',
-          theme: lightTheme(context),
-          darkTheme: darkTheme(),
-          home: const BottomNavigationBarScreen(),
-        );
+        return Builder(builder: (newcontext) {
+          return MaterialApp(
+            builder: (context, child) {
+              return Overlay(
+                initialEntries: [
+                  if (child != null) ...[
+                    OverlayEntry(
+                      builder: (context) => MediaQuery(
+                        data: MediaQuery.of(context).copyWith(
+                          textScaler: const TextScaler.linear(1.0),
+                        ),
+                        child: GestureDetector(
+                          onTap: () =>
+                              FocusManager.instance.primaryFocus?.unfocus,
+                        ),
+                      ),
+                    ),
+                  ],
+                ],
+              );
+            },
+            debugShowCheckedModeBanner: false,
+            title: 'RoomFinder',
+            theme: lightTheme(context),
+            darkTheme: darkTheme(),
+            home: const BottomNavigationBarScreen(),
+          );
+        });
       },
     );
   }
